e4503aecab763970ae7e46dab4ecf802380d6361
smitkovski <stefan_mitkovski@yahoo.com>
2020-04-05 21:44:19 +0200

    coomiit

--- src/test/java/atda/AcceptanceTestDrivenAutomationTests.java
+++ src/test/java/atda/AcceptanceTestDrivenAutomationTests.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import po.LoginPage;
+import po.ProductPage;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class AcceptanceTestDrivenAutomationTests {
@@ -19,24 +22,35 @@ public class AcceptanceTestDrivenAutomationTests {
 
 
     @Test
-    public void shouldOpen() {
+    public void shouldOpen() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
         assertTrue(loginPage.isLoaded());
     }
 
     @Test
-    public void shouldLogin() {
+    public void shouldLogin() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
         assertTrue(loginPage.isLoaded());
-        loginPage.login("standard_user", "standard_password");
+        loginPage.login("standard_user", "secret_sauce");
         assertTrue(new ProductPage(driver).isLoaded());
 
     }
 
+    @Test
+    public void canNotLoginWithInvalidUserName() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        assertTrue(loginPage.isLoaded());
+        loginPage.login("standard_user", "secret");
+        String errorMessage = loginPage.getErrorMessage();
+        assertEquals(errorMessage, "this");
+
+    }
+
     private WebDriver getDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Users/stefan/Desktop/frameworks/SeleniumYoutube/src/main/resources/drivers/chromedriver");
         return new ChromeDriver();
     }
 
